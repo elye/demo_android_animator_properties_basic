9c3f84829e244c6f5deb6c1130330f407bca3a96
elye <elye.project@gmail.com>
2020-06-03 23:21:44 +1000

    Change to basic HAPPY DAY

--- app/src/main/java/com/elyeproj/androidanimator/MainActivity.kt
+++ app/src/main/java/com/elyeproj/androidanimator/MainActivity.kt
@@ -27,34 +27,15 @@ class MainActivity : AppCompatActivity() {
         valueAnimator.duration = 10000
         valueAnimator.addUpdateListener { animation ->
             val progress = animation.animatedValue as Float
-            text_me_5.rotationX = progress - 100
-            text_me_4.rotationX = progress - 80
-            text_me_3.rotationX = progress - 60
-            text_me_2.rotationX = progress - 40
-            text_me_1.rotationX = progress - 20
             text_me.rotationX = progress
-            text_me2.rotationX = progress + 20
-            text_me3.rotationX = progress + 40
-            text_me4.rotationX = progress + 60
-            text_me5.rotationX = progress + 80
-            text_me6.rotationX = progress + 100
-
             text_arrow.rotationY = progress
-
-            view_ball.rotation = progress
-            view_ball.scaleX = progress / 3600 + 1
-            view_ball.scaleY = progress / 3600 + 1
-            view_ball.z = progress
             view_fan.rotation = progress
             view_cloud.alpha = (3600  - progress)/3600
             view_sun.scaleX = progress/3600 + 1
             view_sun.scaleY = progress/3600 + 1
-
             view_hotair.translationY = - progress/10
             view_yatch.translationX = progress/10
             btn_press_me.translationZ = progress/50
-
-
         }
         valueAnimator.start()
     }
